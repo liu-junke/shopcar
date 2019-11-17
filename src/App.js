93c6fab2916af935ed7750ef6b300c3f8670f782
liu-junke <449316100@qq.com>
2019-11-17 17:25:16 +0800

    shopcar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import LayOut from './layout';
+
+function App() {
+  return (
+    <div className="App">
+    <LayOut></LayOut>
+    </div>
+  );
+}
+
+export default App;
